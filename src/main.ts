6c0e6823b100e3f896f0ddc475438e847c82c33d
ono hiroko <azazabc123@gmail.com>
2019-02-09 19:08:20 +0800

    [main.js] main functions implemented

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,73 @@
+interface SearchEngine {
+    /** duckduckgo */
+    id: string,
+    /** DuckDuckGo */
+    name: string,
+    hostname: string,
+    /** `q` in ?q= */
+    queryKey: string,
+    /** https://duckduckgo.com/?q={} */
+    queryUrl: string,
+}
+
+interface CurrentState {
+    keyword: string,
+    currentEngine: SearchEngine,
+    nextEngine: SearchEngine,
+}
+
+const ENGINES: SearchEngine[] = [
+    {
+        id: 'duckduckgo',
+        name: 'DuckDuckGo',
+        hostname: 'duckduckgo.com',
+        queryKey: 'q',
+        queryUrl: 'https://duckduckgo.com/?q={}',
+    },
+    { 
+        id: 'startpage',
+        name: 'StartPage',
+        hostname: 'www.startpage.com',
+        queryKey: 'query',
+        queryUrl: 'https://www.startpage.com/do/dsearch/?query={}',
+    },
+    { 
+        id: 'bing',
+        name: 'Bing',
+        hostname: 'www.bing.com',
+        queryKey: 'q',
+        queryUrl: 'https://www.bing.com/search?q={}',
+    },
+    { 
+        id: 'google',
+        name: 'Google',
+        hostname: 'www.google.com',
+        queryKey: 'q',
+        queryUrl: 'https://www.google.com/search?q={}',
+    },
+]
+
+function getCurrentState (currentUrl?: string): CurrentState | null {
+    if (!currentUrl) {return null}
+    const urlObj = new URL(currentUrl + '')
+    const curIdx = ENGINES.findIndex(x => x.hostname === urlObj.hostname)
+    if (curIdx === -1) { return null }
+    const curEng = ENGINES[curIdx]
+    const params = new URLSearchParams(urlObj.search)
+    const keyword = params.get(curEng.queryKey) || ''
+    const nextEng = ENGINES[(curIdx + 1) % ENGINES.length]
+    return {
+        keyword: keyword,
+        currentEngine: curEng,
+        nextEngine: nextEng
+    }
+}
+
 browser.pageAction.onClicked.addListener(function (tab) {
-    const url = new URL(tab.url + '')
-    const params = new URLSearchParams(url.search)
-    console.log('hello', params.get('q'))
+    const state = getCurrentState(tab.url)
+    if (!state) {return console.error('[To Developer] This should not happen.')}
+    browser.tabs.update(tab.id, {
+        url: state.nextEngine.queryUrl.replace(/{}/, state.keyword)
+    })
 })
 
